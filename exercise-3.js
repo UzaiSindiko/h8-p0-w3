4b52a3c6ff5a9519bc9ec1739580cb05b8b1cf2d
uzaisindiko <uzai.sindiko@gmail.com>
2019-07-09 16:12:39 +0700

    [Exercises 3] Tantangan Array 2 (Melooping menggunakan Array)

--- exercise-3.js
+++ exercise-3.js
@@ -1,16 +1,44 @@
 var input = [
-	[ '0001', 'Roman Alamsyah', 'Bandar Lampung', '21/05/1989', 'Membaca' ],
-	[ '0002', 'Dika Sembiring', 'Medan', '10/10/1992', 'Bermain Gitar' ],
-	[ '0003', 'Winona', 'Ambon', '25/12/1965', 'Memasak' ],
-	[ '0004', 'Bintang Senjaya', 'Martapura', '6/4/1970', 'Berkebun' ]
+	[
+		'0001',
+		'Roman Alamsyah',
+		'Bandar Lampung',
+		'21/05/1989',
+		'Membaca'
+	],
+	[
+		'0002',
+		'Dika Sembiring',
+		'Medan',
+		'10/10/1992',
+		'Bermain Gitar'
+	],
+	[
+		'0003',
+		'Winona',
+		'Ambon',
+		'25/12/1965',
+		'Memasak'
+	],
+	[
+		'0004',
+		'Bintang Senjaya',
+		'Martapura',
+		'6/4/1970',
+		'Berkebun'
+	]
 ];
 
 function dataHandling(inputs) {
-	var label = [ 'Nomor ID:', 'Nama Lengkap:', 'TTL:', 'Hobi:' ];
+	var label = [
+		'Nomor ID:',
+		'Nama Lengkap:',
+		'TTL:',
+		'Hobi:'
+	];
 	var data = '';
 	for (var i = 0; i < inputs.length; i++) {
 		var k = 0;
-
 		for (var j = 0; j < label.length; j++) {
 			data += inputs[i][k];
 
